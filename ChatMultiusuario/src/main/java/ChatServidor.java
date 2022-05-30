34fa72254b1df58bfc1eaecb3cb18739871a84e9
normanagudelo <63974062+normanagudelo@users.noreply.github.com>
2022-05-29 23:41:13 -0500

    ChatMultiusuario
    Chat para varios usuarios de la misma red , construido en su totalidad con JAVA

---
+++ ChatMultiusuario/src/main/java/ChatServidor.java
@@ -0,0 +1,121 @@
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ChatServidor extends javax.swing.JFrame implements Runnable {
+  
+    public ChatServidor() {
+        initComponents();
+        Thread hilo=new Thread (this);
+        hilo.start();
+    }
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextAreaServidor = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jTextAreaServidor.setColumns(20);
+        jTextAreaServidor.setRows(5);
+        jScrollPane1.setViewportView(jTextAreaServidor);
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel2.setText("SERVIDOR");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(26, 26, 26)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 339, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(151, 151, 151)
+                        .addComponent(jLabel2)))
+                .addContainerGap(35, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(22, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 196, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(45, 45, 45))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+   
+    public static void main(String args[]) {
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ChatServidor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ChatServidor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ChatServidor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ChatServidor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ChatServidor().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextAreaServidor;
+    // End of variables declaration//GEN-END:variables
+
+
+    @Override
+    public void run(){
+
+try {
+    ServerSocket servidor=new ServerSocket(1003);
+    while(true)
+    {   System.out.println("Estoy dentro de while de servidor");
+        Socket sc=servidor.accept();
+        DataInputStream recibido=new DataInputStream(sc.getInputStream());
+        String datorecibido=recibido.readUTF();
+        jTextAreaServidor.append("\n"+datorecibido);  
+        
+        DataOutputStream enviado=new DataOutputStream(sc.getOutputStream());
+        enviado.writeUTF("Servidor confirma recepción del mensaje:"+datorecibido);
+        
+        System.out.println("Estoy dentro de while de servidor");
+ 
+    }
+        
+}catch (Exception ex)
+{
+System.out.println(ex);
+}        
+}
+}
